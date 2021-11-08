d2f2614c7d48922e6d7504b1b601fbac26462ccb
ccn <ccn@cuppajoeman.com>
2021-11-08 17:22:48 -0500

    this was an automated push

---
+++ shell-progs/pusher.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Declare an array of string with type
+declare -a StringArray=("~/knowledge-book" "~/basic-system" )
+ 
+# Iterate the string array using for loop
+for val in ${StringArray[@]}; do
+   cd val
+   git add -A && git commit -m "this was an automated push" && git push
+done
+
+#cd ~/knowledge-book
+#git add -A && git commit -m "this was an automated push" && git push
---
+++ shell-progs/remove_xfce_keyboard_shortcuts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml 
