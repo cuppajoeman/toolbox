3c3c2e01630ebc2a60dda09472af5c0f6db61b99
cuppajoeman <ccn@cuppajoeman.com>
2022-10-20 20:07:44 -0400

    update homepage

--- configuration/configure.sh
+++ configuration/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sh ~/basic-system/configuration/keyboard/switch_escape_and_caps.sh
+sh ~/my-system/configuration/keyboard/switch_escape_and_caps.sh
 
-sh ~/basic-system/configuration/mouse-settings/zowie_gaming_mouse_setup.sh
+sh ~/my-system/configuration/mouse-settings/zowie_gaming_mouse_setup.sh
 
-sh ~/basic-system/configuration/display/vga_to_1080.sh
+#sh ~/my-system/configuration/display/vga_to_1080.sh
 
