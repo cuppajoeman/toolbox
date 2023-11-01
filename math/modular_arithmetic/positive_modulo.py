b9c24b4b3ad0a287f26600ae247266bdb4834b91
ccn <ccn@cuppajoeman.com>
2023-11-01 01:41:15 -0400

    refactor and start eleventy integration

--- math/modular_arithmetic/positive_modulo.py
+++ math/modular_arithmetic/positive_modulo.py
@@ -4,4 +4,4 @@ then add m to it, the reason why we take the mod again is because if
 x % m >= 0, then adding m pushes it out of the range again
 """
 def pos_mod(x,m):
-    return (x%m + m)%m;
+    return (x%m + m)%m
